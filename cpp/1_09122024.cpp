7b52e0a76011d5d1a62c7e4138337eaec2f113be
Seungje <davidseong8914@gmail.com>
2024-12-13 18:33:52 -0500

    1

---
+++ cpp/1_09122024.cpp
@@ -0,0 +1,31 @@
+/*
+1. Two Sum
+
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+*/
+
+// Brute force
+#include <iostream>
+#include <vector>
+
+class Solution {
+std::vector<int> twosum;
+public:
+    std::vector<int> twoSum(std::vector<int> &nums, int target) {
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = i+1; j < nums.size(); j++) {
+                if (nums[i] + nums[j] == target) {
+                    twosum.push_back(i);
+                    twosum.push_back(j);
+                    return twosum;
+                }
+            }
+        }
+        return twosum;
+    }
+};
