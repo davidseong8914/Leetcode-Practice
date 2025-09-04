5dfd432f155d1cf23dde39018b3259263beba4da
Seungje <davidseong8914@gmail.com>
2025-09-04 10:12:03 -0400

    151 most optimal

--- 75_interview/151_20250904.py
+++ 75_interview/151_20250904.py
@@ -1,20 +1,6 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
+        word_list = s.split()
+        return ' '.join(reversed(word_list))
 
-        word_list = s.split(' ')
-        new_word_list = []
-
-        print(word_list)
-        for i in reversed(word_list):
-            if i != '':
-                new_word_list.append(i)
-                new_word_list.append(' ')
-            else:
-                pass
-        
-        new_word = ''.join(new_word_list)
-        new_word = new_word.strip()
-
-        return new_word
-
-        
+            
