37229f7d5e6f3afcc17183709c2eb87f97006745
Seungje <davidseong8914@gmail.com>
2025-10-03 23:59:20 -0400

    735 attempted

---
+++ 75_interview/735_20251003.py
@@ -0,0 +1,11 @@
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        survived_asteroids = []
+        for i in range(len(asteroids)):
+            if len(survived_asteroids) == 0:
+                pass
+            elif asteroids[i] > 0:
+                survived_asteroids.append
+
+
+        
