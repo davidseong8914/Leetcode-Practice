a7e86dcc89a630f2359b89e398a7e44aa8beeb11
Seungje <davidseong8914@gmail.com>
2025-10-06 08:25:35 -0400

    735 solved, close to optimal

--- 75_interview/735_20251003.py
+++ 75_interview/735_20251003.py
@@ -2,10 +2,23 @@ class Solution:
     def asteroidCollision(self, asteroids: List[int]) -> List[int]:
         survived_asteroids = []
         for i in range(len(asteroids)):
-            if len(survived_asteroids) == 0:
-                pass
-            elif asteroids[i] > 0:
-                survived_asteroids.append
+            if not(survived_asteroids) or not(survived_asteroids[-1] > 0 and asteroids[i] < 0):
+                survived_asteroids.append(asteroids[i])
+            else:
+                while survived_asteroids and abs(asteroids[i]) > survived_asteroids[-1] and survived_asteroids[-1] > 0:
+                    survived_asteroids.pop()
+
+                if not(survived_asteroids) or survived_asteroids[-1] < 0:
+                    survived_asteroids.append(asteroids[i])
+                elif survived_asteroids[-1] == abs(asteroids[i]):
+                    survived_asteroids.pop()
+                else:
+                    pass
+
+                
+
+
+        return survived_asteroids
 
 
         
