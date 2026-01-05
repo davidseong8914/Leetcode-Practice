e84d857d1b1f6f15be27e951b309ff51e148fff2
Seungje <davidseong8914@gmail.com>
2026-01-05 09:37:42 -0500

    solved - list

--- quest/Q2_array_II.py
+++ quest/Q2_array_II.py
@@ -20,3 +20,25 @@ class Solution(object):
             ret_list.append(ret_dic[i])
 
         return ret_list
+
+        # list method
+
+        # freq = [0] * 101
+
+        # for num in nums:
+        #     freq[num] += 1
+        
+        # cur_sum = 0
+
+        # smaller = [0] * 101
+        # for i in range(len(smaller)):
+        #     smaller[i] = cur_sum
+        #     cur_sum += freq[i]
+
+        # ret_list = []
+
+        # for i in nums:
+        #     ret_list.append(smaller[i])
+
+        # return ret_list
+
