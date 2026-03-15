fce015df9f83a8b6f966687b14cc97db8904fb05
Seungje <davidseong8914@gmail.com>
2026-03-15 09:09:50 -0400

    boto3

---
+++ AWS/Boto3_DynamoDB.py
@@ -0,0 +1,27 @@
+# set up IAM
+# set up table
+
+import sys
+import datetime
+import time
+import boto3
+
+db = boto3.resource('dynamodb', region_name='us-east-1')
+table = db.Table("test_table")
+
+# table.put_item(
+#     Item = {
+#         'test_id': "3", # important
+#         'name'  : "San Andres",
+#         'Age'   : "24" 
+#     }
+# )
+
+
+response = table.get_item(
+    Key = {
+        'test_id' : "1"
+    }
+)
+# print(response['Item'])
+print(response)
